0c0708da204d9f0615308c6a0dd3640548bf2be2
Billy Huang <10646003@ntub.edu.tw>
2022-01-21 16:36:18 +0800

    refactor: adjust directories and files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import type { AppProps } from 'next/app';
 
-import wrapper from '@store/store';
+import wrapper from '@store';
 import '@styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
